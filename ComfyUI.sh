c125af7f96ceb1770ba6a66cbd1beeccad86e66d
Alucard Stormbringer <supersayan1000@gmail.com>
2025-05-12 23:36:21 +0530

    script: ComfyUI: rework script

--- ComfyUI.sh
+++ ComfyUI.sh
@@ -1,7 +1,18 @@
+# Install Python venv
 sudo apt install python-is-python3 python3.12-venv
-git clone https://github.com/comfyanonymous/ComfyUI.git --depth=1 ~/ComfyUI
-cd ~/ComfyUI
-python -m venv .venv
-source .venv/bin/activate
-pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu126
-pip install -r requirements.txt
+
+# Clone ComfyUI only if it doesn't exist
+if [ ! -d "$HOME/ComfyUI" ]; then
+    git clone https://github.com/comfyanonymous/ComfyUI.git --depth=1 ~/ComfyUI
+fi
+
+# Move into the ComfyUI directory
+cd "$HOME/ComfyUI"
+
+# Setup only if in the correct directory
+if [ "$PWD" = "$HOME/ComfyUI" ]; then
+    python -m venv .venv
+    source .venv/bin/activate
+    pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu126
+    pip install -r requirements.txt
+fi
